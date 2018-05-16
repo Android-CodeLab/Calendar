9386c0a9f89a96b01295c89c7496b00525b1d0af
LibJamun <jatinsahgal50@gmail.com>
2018-05-16 11:38:11 +0530

    0.0.1(v1)

---
+++ README.md
@@ -0,0 +1,8 @@
+<h1><b>Jamun-Calendar</b></h1>
+<h3>Calendar is a collection of Beautiful Activities which help others to make there Fully Custom Calendar View with Single and Multi Date Picker Functionality.</h3>
+<br>
+<h1><b>Hi Developers</b></h1>
+<b>Here,</b><br>
+  <b>1.</b>
+  
+  
---
+++ sample/src/main/java/tk/jamun/sample/ActivityScannerSample.java
@@ -0,0 +1,18 @@
+package tk.jamun.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class ActivityScannerSample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner_sample);
+    }
+
+    public void onClickScanner(View view) {
+
+    }
+}
